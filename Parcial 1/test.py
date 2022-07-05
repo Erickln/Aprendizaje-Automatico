04a8a95ac16ac6cea30fc8b277b963e7172a8c25
Erickln <erickelnd@gmail.com>
2022-07-05 10:31:53 -0500

    first push

---
+++ Parcial 1/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+# imporr scikit_learn as sk
+
+# processed.switerland.data has data in this format:
+
+# Not mutable variable
+# Documentation: \assets\heart-disease.names
+DATA_PATH = 'assets\\diabetes.csv'
+
+
+# creating a dataframe from the data with the columns:
+
+df = 
+
+
+
+
